96a4faf311f994c7c557e6ca039c3294073751ad
lordvictor6393 <lordvictor6393@gmail.com>
2018-07-18 08:06:56 -0400

    adding angular material to client add/edit (pending projects table)

--- src/app/client/c-add-edit/c-add-edit.component.ts
+++ src/app/client/c-add-edit/c-add-edit.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Client } from '../client.model';
 import { ClientService } from '../client.service';
+import { Project } from '../../project/project.model';
+import { MatTableDataSource, MatSort, MatTable } from '../../../../node_modules/@angular/material';
+import { ProjectService } from '../../project/project.service';
+import { UsersService } from '../../user/user.service';
 
 @Component({
   selector: 'app-c-add-edit',
@@ -12,22 +16,34 @@ import { ClientService } from '../client.service';
 export class CAddEditComponent implements OnInit {
 
   isNew: boolean = false;
-  clientForm: FormGroup;
-  selectedClient: Client;
   selectedClientId: string;
+  initialClientData: Client;
+
+  projects: Project[];
+  clientForm: FormGroup;
+
+  pGridColumns: string[];
+  clientProjects: Project[];
+  projDataSource: MatTableDataSource<Project> = new MatTableDataSource<Project>();
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatTable) projectsTable: MatTable<Project>;
 
   constructor(private route: ActivatedRoute,
               private router: Router,
+              private projectService: ProjectService,
+              private userService: UsersService,
               private clientService: ClientService) { }
 
   ngOnInit() {
+    this.projectService.getProjectList().subscribe(
+      projList => this.projects = projList
+    );
     this.clientForm = new FormGroup({
-      'clientName': new FormControl(null),
-      'contactInfo': new FormGroup({
-        'contactName': new FormControl(null),
-        'contactPhone': new FormControl(null)
-      }),
-      'clientProjects': new FormControl(null)
+      'name': new FormControl(null),
+      'contactDetails': new FormGroup({
+        'name': new FormControl(null),
+        'phone': new FormControl(null)
+      })
     });
     this.route.params.subscribe(
       params => {
@@ -39,40 +55,50 @@ export class CAddEditComponent implements OnInit {
         }
       }
     )
+    this.projDataSource.sort = this.sort;
+    this.pGridColumns = ['code', 'name', 'lead'];
   }
 
   loadClient() {
     this.clientService.getClient(this.selectedClientId).subscribe(
       (client) => {
-        this.selectedClient = client;
+        this.initialClientData = client;
         this.clientForm.setValue({
           clientName: client.name,
-          clientProjects: client.projectsIds.join(','),
           contactInfo:{
             contactName: client.contactDetails.name,
             contactPhone: client.contactDetails.phone
           }
         });
+        this.clientProjects = client.projectsIds.map(
+          projId => {
+            let projInstance = this.projects.find(
+              project => project.id == projId
+            );
+            return projInstance;
+          }
+        );
+        if(this.clientProjects.length) {
+          this.projDataSource.data = this.clientProjects;
+        }
       }
     )
   }
 
-  onClientSubmit() {
+  onSaveClient() {
     let formValues = this.clientForm.value;
     let clientData = {
       name: formValues.clientName,
-      projectsIds: formValues.clientProjects.split(','),
       contactDetails: {
         name: formValues.contactInfo.contactName,
         phone: formValues.contactInfo.contactPhone
-      }
+      },
     };
     if(this.isNew) {
       this.clientService.addClient(clientData);
     } else {
       this.clientService.updateClient(this.selectedClientId, clientData);
     }
-    this.clientForm.reset();
     this.backToList();
   }
 
